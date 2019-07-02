ec220708174eeee0adb6ff0eac581bb2d8e4f255
samet irkören <sametirkoren@gmail.com>
2019-07-02 19:06:33 +0300

    Arayüz tasarımı oluşturuldu.

--- www/js/app.js
+++ www/js/app.js
@@ -6,72 +6,72 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs).
-    // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
-    // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
-    // useful especially with forms, though we would prefer giving the user a little more room
-    // to interact with the app.
-    if (window.cordova && window.Keyboard) {
-      window.Keyboard.hideKeyboardAccessoryBar(true);
-    }
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs).
+      // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
+      // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
+      // useful especially with forms, though we would prefer giving the user a little more room
+      // to interact with the app.
+      if (window.cordova && window.Keyboard) {
+        window.Keyboard.hideKeyboardAccessoryBar(true);
+      }
 
-    if (window.StatusBar) {
-      // Set the statusbar to use the default style, tweak this to
-      // remove the status bar on iOS or change it to use white instead of dark colors.
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
+      if (window.StatusBar) {
+        // Set the statusbar to use the default style, tweak this to
+        // remove the status bar on iOS or change it to use white instead of dark colors.
+        StatusBar.styleDefault();
+      }
+    });
   })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
 
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
+      .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+      })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
+      .state('app.kurallar', {
+        url: '/kurallar',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/kurallar.html'
+          }
+        }
+      })
+
+      .state('app.oyun', {
+        url: '/oyun',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/oyun.html'
+          }
+        }
+      })
+      .state('app.anasayfa', {
+        url: '/anasayfa',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/anasayfa.html',
+            controller: 'AnasayfaCtrl'
+          }
+        }
+      })
+
+      .state('app.single', {
+        url: '/playlists/:playlistId',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlist.html',
+            controller: 'PlaylistCtrl'
+          }
+        }
+      });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/anasayfa');
   });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,133 @@
-angular.module('starter.controllers', [])
+angular.module("starter.controllers", [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  .controller("AppCtrl", function ($scope, $ionicModal, $timeout, $interval) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    $scope.saniye = 30;
+    $scope.hak = 3;
+    $scope.skor = 0;
+    $scope.isDisabled = true;
+    $scope.isHidden = true;
 
-  // Form data for the login modal
-  $scope.loginData = {};
+    $scope.goster = function (kelime) {
+      $scope.kelime = kelime;
+      console.log("Kelimeniz : ", $scope.kelime);
+      $scope.bitis = $scope.kelime.substr(-1);
+      console.log("Kelimeniz", $scope.bitis, " harfi ile bitiyor.");
+      if ($scope.bitis == $scope.kelime.substr(0, 1)) {
+        console.log("Doğru bildiniz!");
+        $scope.skor += 1;
+        angular.element("#skorAnimasyon").addClass("animated bounce");
+        angular.element("#border").addClass("dogruBorder");
+        angular.element("#dogruCevap").addClass("dogruCevap");
+        document.getElementById("fokuslan").value = "";
+        var sil = $interval(function () {
+          angular.element("#skorAnimasyon").removeClass("animated bounce");
+          angular.element("#border").removeClass("dogruBorder");
+          angular.element("#dogruCevap").removeClass("dogruCevap");
+          $interval.cancel(sil);
+        }, 1000);
+      } else {
+        console.log("Yanlış bildiniz...");
+        $scope.skor -= 1;
+        $scope.hak -= 1;
+        angular.element("#hak").addClass("hakRenk");
+        angular.element("#yanlisCevapAnimasyon").addClass("animated shake");
+        angular.element("#border").addClass("yanlisBorder");
+        document.getElementById("fokuslan").value = "";
+        var sil2 = $interval(function () {
+          angular
+            .element("#yanlisCevapAnimasyon")
+            .removeClass("animated shake");
+          angular.element("#border").removeClass("yanlisBorder");
+          angular.element("#hak").removeClass("hakRenk");
+          $interval.cancel(sil2);
+        }, 1000);
+      }
+    };
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+    $scope.basla = function () {
+      console.log("oyun başladı");
+      $scope.isDisabled = false;
+      angular
+        .element("#yanlisCevapAnimasyon")
+        .removeClass("animated bounceInLeft");
+      angular.element("#skorAnimasyon").removeClass("animated heartBeat");
+      angular.element("#opacity").removeClass("modalOpacity");
+      $timeout(function () {
+        document.getElementById("fokuslan").select();
+        document.getElementById("fokuslan").value = "";
+      }, 0);
+      var sure = $interval(function () {
+        $scope.saniye -= 1;
+        if ($scope.saniye < 6) {
+          angular.element("#sureRenk").addClass("sureRenk");
+        }
+        if ($scope.saniye == 0 || $scope.hak == 0) {
+          console.log("oyun bitti");
+          $scope.skorOgren = $scope.skor;
+          $interval.cancel(sure);
+          $scope.saniye = 30;
+          $scope.skor = 0;
+          $scope.isDisabled = true;
+          $scope.hak = 3;
+          document.getElementById("fokuslan").value = "";
+          $scope.bitis = "";
+          angular.element("#sureRenk").removeClass("sureRenk");
+          angular.element("#opacity").addClass("modalOpacity");
+          angular.element("#skorGoster").addClass("goster");
+          $scope.isHidden = false;
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+          $scope.kapat = function () {
+            angular.element("#skorGoster").removeClass("goster");
+            angular.element("#opacity").removeClass("modalOpacity");
+            $scope.isHidden = true;
+          }
+        }
+      }, 1000);
+    };
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+    // Form data for the login modal
+    $scope.loginData = {};
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    // Create the login modal that we will use later
+    $ionicModal
+      .fromTemplateUrl("templates/login.html", {
+        scope: $scope
+      })
+      .then(function (modal) {
+        $scope.modal = modal;
+      });
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function () {
+      $scope.modal.hide();
+    };
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+    // Open the login modal
+    $scope.login = function () {
+      $scope.modal.show();
+    };
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function () {
+      console.log("Doing login", $scope.loginData);
+
+      // Simulate a login delay. Remove this and replace with your login
+      // code if using a login system
+      $timeout(function () {
+        $scope.closeLogin();
+      }, 1000);
+    };
+  })
+
+  .controller("AnasayfaCtrl", function ($scope) {
+
+  })
+
+  .controller("PlaylistCtrl", function ($scope, $stateParams) {});
